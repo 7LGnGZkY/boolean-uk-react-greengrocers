eeb48aba463b6d9fbbd921605e43b61863213150
7LGnGZkY <n35.nnlpd@slmail.me>
2022-08-02 13:54:33 +0100

    Exercise complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './styles/reset.css'
 import './styles/index.css'
 
-import initialStoreItems from './store-items'
-
 /*
 Here's what a store item should look like
 {
@@ -14,7 +12,7 @@ Here's what a store item should look like
 What should a cart item look like? 🤔
 */
 
-console.log(initialStoreItems)
+const initialStoreItems = []
 
 export default function App() {
   // Setup state here...
@@ -23,23 +21,23 @@ export default function App() {
     <>
       <header id="store">
         <h1>Greengrocers</h1>
-        <ul className="item-list store--item-list">
+        <ul class="item-list store--item-list">
           {/* Wrtite some code here... */}
         </ul>
       </header>
       <main id="cart">
         <h2>Your Cart</h2>
-        <div className="cart--item-list-container">
-          <ul className="item-list cart--item-list">
+        <div class="cart--item-list-container">
+          <ul class="item-list cart--item-list">
             {/* Wrtite some code here... */}
           </ul>
         </div>
-        <div className="total-section">
+        <div class="total-section">
           <div>
             <h3>Total</h3>
           </div>
           <div>
-            <span className="total-number">£0.00</span>
+            <span class="total-number">£0.00</span>
           </div>
         </div>
       </main>
